b3d4f31c809536a850c932bc874c93487100c2f1
MD Waliullah <waliullah.godev@gmail.com>
2026-02-15 10:08:32 +0600

    adjacency list

--- module-1/adjacency_matrix_of_undirected_graph.cpp
+++ module-1/adjacency_matrix_of_undirected_graph.cpp
@@ -9,6 +9,11 @@ int main()
     int adj_mat[n][n];
     memset(adj_mat, 0, sizeof(adj_mat));
 
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            if (i == j)
+                adj_mat[i][j] = 1;
+
     for (int i = 0; i < e; i++)
     {
         int a, b;
