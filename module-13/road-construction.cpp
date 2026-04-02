9d393d6e1e9f6847e1ec2ae5c4ecb0016bd53957
MD Waliullah <waliullah.godev@gmail.com>
2026-04-02 08:57:28 +0600

    corner_case handle

--- module-13/road-construction.cpp
+++ module-13/road-construction.cpp
@@ -21,6 +21,9 @@ void dsu_union(int node1, int node2)
     int leader1 = find(node1);
     int leader2 = find(node2);
 
+    if(leader1 == leader2)
+        return;
+
     if (par[leader1] >= par[leader2])
     {
         par[leader2] = leader2;
