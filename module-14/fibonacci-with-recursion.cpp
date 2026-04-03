7e0c9f39b0e17029aa4cc73b38c6a6e5d86dddc7
MD Waliullah <waliullah.godev@gmail.com>
2026-04-03 08:41:05 +0600

    fibonacci-with-recursion

---
+++ module-14/fibonacci-with-recursion.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fibo(int n)
+{
+    if (n < 2)
+        return n;
+
+    return (fibo(n - 1) + fibo(n - 2));
+}
+
+int main()
+{
+
+    int val = fibo(5);
+    cout << val;
+    return 0;
+}
