062ad74c7f25bac33fbab4011af77094009cafbb
MD Waliullah <waliullah.godev@gmail.com>
2026-04-03 08:06:34 +0600

    factorial-with-recursion

---
+++ module-14/factorial-with-recursion.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fac(int n)
+{
+    if (n == 1)
+        return 1;
+
+    return (n * fac(n - 1));
+}
+
+int main()
+{
+
+    int val = fac(5);
+    cout << val;
+    return 0;
+}
